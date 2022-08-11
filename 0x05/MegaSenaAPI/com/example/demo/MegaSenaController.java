45526cab35e19b539e893a0a9788bbc9a3a3f455
LaizaZnCIT <laiza@ciandt.com>
2022-08-11 09:39:17 -0300

    Spring Security task 0

---
+++ 0x05/MegaSenaAPI/com/example/demo/MegaSenaController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/megasena")
+public class MegaSenaController {
+	
+	@GetMapping("/simpleMessageWelcome")
+	   public String mensagemBoasVindas() {
+	        return "Bem-vindo a MegaSena!";
+	   }
+
+
+	   @GetMapping("/getNumbers")
+	   public List<Integer> numerosMegaSena() {
+	        Random random = new Random();
+	        List<Integer> numerosGerados = new ArrayList<>();
+	        
+	        for(int i = 0; i < 14; i++) {
+	        	numerosGerados.add(random.nextInt(100));
+	        }
+	        //Obs: Retornar os números gerados em ordem crescente. 
+	        Collections.sort(numerosGerados);
+	        return numerosGerados;
+	   }
+}
