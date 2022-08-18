f4f9c2cb51fd4d1da7e196a2632e47633ad1bd1c
LaizaZnCIT <laiza@ciandt.com>
2022-08-18 14:59:45 -0300

    Documentando API's com Swagger

---
+++ 0x08/ProductAPI/src/main/java/com/example/product/model/ProductRepository.java
@@ -0,0 +1,37 @@
+package com.example.product.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepository {
+	private List<Product> list = new ArrayList<Product>();
+
+	  public List<Product> getAllProducts() {
+	    return list;
+	  }
+
+	  public Product getProductById(Long id) {
+	    return list.stream().filter(p -> id.equals(p.getId())).findAny().orElse(null); 
+	  }
+
+	  public void addProduct(Product s) {
+	    list.add(s);
+	  }
+
+	  public void updateProduct(Product s) {
+	    int index = list.indexOf(s);
+	    list.set(index, s);
+	   }
+
+	   public void removeProduct(Product s) {
+	     list.remove(s);
+	   }
+
+	   public void addList(List<Product> listOfProducts) {
+	     list.addAll(listOfProducts);
+	   }
+
+}
